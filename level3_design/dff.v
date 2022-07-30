fa28ee7d54386725d974916597f258b134dd18f8
Saran1210 <109662288+Saran1210@users.noreply.github.com>
2022-07-30 12:28:30 +0530

    Update dff.v

--- level3_design/dff.v
+++ level3_design/dff.v
@@ -1,10 +1,22 @@
-module dff (
-  input clk, d,
-  output wire q
-);
+module dff #
+ (
+  parameter WIDTH = 8
+ )
 
-always @ (posedge clk) begin
-  q <= d;
-end
+ (
+  input [0:0] CLK, 
+  input [0:0] RST, 
+  input [(WIDTH - 1):0] D, 
+  output reg [(WIDTH - 1):0] Q
+ );
 
+  always @(posedge CLK)
+  begin        
+  if(RST) begin        
+  Q <= 0;
+  end  
+  else begin        
+  Q <= D;
+  end 
+  end 
 endmodule
